bfc55b9f802f5df2a3cd772f4ca670677f7fa845
Manglemix <shabouza030@gmail.com>
2023-02-06 13:07:39 -0700

    Remove mangledb

--- mangledb/src/lib.rs
+++
@@ -1,9 +0,0 @@
-#![feature(maybe_uninit_uninit_array)]
-#![feature(maybe_uninit_array_assume_init)]
-
-pub mod error;
-pub mod conn;
-pub mod serde;
-pub mod sync;
-pub mod db;
-pub mod unique_owned;
